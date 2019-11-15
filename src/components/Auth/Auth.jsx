c18b1c3ab12528a8a227667bd7502e055373d0ef
WillHoll <willhollingsworth15@yahoo.com>
2019-11-15 09:35:08 -0700

    setup complete

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div>
+        Auth.jsx
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        Post.jsx
+      </div>
+    );
+  }
+}
+
+export default Post;
